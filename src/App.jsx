19e91a8c8aee73ee81b2c23accfb2db19851fb17
simonlundmark1 <155955074+simonlundmark1@users.noreply.github.com>
2024-03-14 09:24:05 +0100

    hej

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import Header from './header.jsx'; // Make sure the path to Header.js is correct
-import Categories from './Categories.jsx'; // Import the Categories component
-import Job from './Job'; // Import the Job component
+import Header from './header.jsx'; 
+import Categories from './Categories.jsx'; 
+import Job from './Job'; 
 import { jobs } from './jobsData';
-import './App.css'; // Or the path to your global CSS file
-
+import './App.css'; 
 
 const mainContainerStyle = {
   maxWidth: '1024px',
-  margin: '-6px 6px 6px 25%', // This centers the div
-  backgroundColor: 'white', // Or any color for the content background
-  // Additional styles as needed
+  margin: '-6px 6px 6px 25%', 
+  backgroundColor: 'white',
+ 
 };
-
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
       <Header />
       <Categories /> {/* Place the Categories component just below the Header component */}
       {jobs.map(job => (
-        <Job key={job.id} job={job} /> // Använd 'key' prop för unik identifiering
+        <Job key={job.id} job={job} /> 
       ))}
 
       {/* Other components or content here */}
@@ -29,5 +27,4 @@ function App() {
   );
 }
 
-
-export default App;
+export default App; 
--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,27 +1,27 @@
 function Categories() {
     const categoriesStyle = {
-      height: '120px', // Height set to 120px
-      width: '1012px', // Width set to 100% to take the full width available
-      border: '1px solid black', // Black border at 1px
-      backgroundColor: '#D3E0D1', // Background color set to #D3E0D1
-      margin: '0 auto', // This centers the header horizontally
-      position: 'relative', // This positions your header relative to its normal position; you might not need this depending on your layout
+      height: '120px', 
+      width: '1012px', 
+      border: '1px solid black',
+      backgroundColor: '#D3E0D1', 
+      margin: '0 auto', 
+      position: 'relative', 
       padding: '6px',
-      boxSizing: 'border-box', // Ensure the padding is included in the element's total width and height
-      marginBottom: '6px', // Add a 6px bottom margin
+      boxSizing: 'border-box', 
+      marginBottom: '6px',
     };
 
     
   const textStyle = {
-/*     color: '#FFFFFF', // Set the font color to white
- */    fontFamily: 'Inter, sans-serif', // Specifying the Inter font along with a fallback
-    fontWeight: '700', // Bold font weight
-    fontSize: '19px', // Font size set to 19px
-    textAlign: 'left', // Align text to the left
-    // Ensure text alignment within the component, considering padding
-    lineHeight: '118%', // Center the text vertically, adjust based on your design needs
+/*     color: '#FFFFFF',
+ */    fontFamily: 'Inter, sans-serif', 
+    fontWeight: '700',
+    fontSize: '19px', 
+    textAlign: 'left', 
+   
+    lineHeight: '118%', 
     letterSpacing: '-0.04em',
-    marginLeft: '2px', // Add 5px margin to the left
+    marginLeft: '2px', 
     };
 
 
@@ -29,9 +29,9 @@ function Categories() {
       fontFamily: 'Inter, sans-serif',
       fontSize: '16px',
       color: 'black',
-      marginTop: '-9px', // Lägg till lite utrymme mellan etiketten och dropdownen
+      marginTop: '-9px',
       marginLeft: '2px',
-      marginBottom: '4px', // Minska detta värde för att flytta upp texten lite
+      marginBottom: '4px', 
 
     };
   
@@ -41,20 +41,20 @@ function Categories() {
       height: '30px',
       border: '1px solid black',
       backgroundColor: 'white',
-      marginRight: '6px', // 6px mellanrum mellan dropdown-listorna
+      marginRight: '6px',
     };
 
     const dropdownContainerStyle = {
-      display: 'flex', // Använder Flexbox för att arrangera containers bredvid varandra
-      justifyContent: 'flex-start', // Startar arrangemanget från vänsterkanten
-      gap: '6px', // Lägger till ett mellanrum mellan varje dropdown-container
+      display: 'flex',
+      justifyContent: 'flex-start', 
+      gap: '6px', 
       marginBottom: '6px'
       
   
     };
     const dropdownGroupStyle = {
-      display: 'flex', // Använder Flexbox för att arrangera etiketten ovanför dropdown-listan
-      flexDirection: 'column', // Arrangerar innehållet vertikalt
+      display: 'flex',
+      flexDirection: 'column',
     };
 
 
